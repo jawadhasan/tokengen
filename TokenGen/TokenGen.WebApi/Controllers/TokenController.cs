0d9096182a4bc651e15dd3debafa8dabcb4f9927
Jawad Shani <jawad.shani@hitachi-eu.com>
2024-03-07 20:38:36 +0100

    api code added

---
+++ TokenGen/TokenGen.WebApi/Controllers/TokenController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TokenGen.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new
+            {
+                Guid = Guid.NewGuid().ToString(),
+                Expires = DateTime.Now.AddDays(1),
+                Issuer = Environment.MachineName
+            };
+        }
+    }
+}
